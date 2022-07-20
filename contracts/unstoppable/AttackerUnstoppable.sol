6908fb0fe70f352e1cfa9929f156610d37a3ad0f
garyb9 <gberkinfand@gmail.com>
2022-07-21 01:29:37 +0300

    removing comments

--- contracts/unstoppable/AttackerUnstoppable.sol
+++ contracts/unstoppable/AttackerUnstoppable.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 import "../unstoppable/UnstoppableLender.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -22,11 +22,7 @@ contract AttackerUnstoppable {
 
     // Pool will call this function during the flash loan
     function receiveTokens(address tokenAddress, uint256 amount) external {
-        console.log("Owner balance:", IERC20(tokenAddress).balanceOf(owner));
-
         require(msg.sender == address(pool), "Sender must be pool");
-        console.log("This balance:", IERC20(tokenAddress).balanceOf(address(this)));
-        console.log("Pool balance:", IERC20(tokenAddress).balanceOf(msg.sender));
 
         uint256 owner_balance = IERC20(tokenAddress).balanceOf(owner);
 
@@ -34,9 +30,6 @@ contract AttackerUnstoppable {
 
         // Return all tokens to the pool
         require(IERC20(tokenAddress).transfer(msg.sender, amount), "Transfer of tokens failed");
-        console.log("Owner balance:", IERC20(tokenAddress).balanceOf(owner));
-        console.log("This balance:", IERC20(tokenAddress).balanceOf(address(this)));
-        console.log("Pool balance:", IERC20(tokenAddress).balanceOf(msg.sender));
     }
 
     function executeFlashLoan(uint256 amount) external {
