16aabe6ea367848fec2cfa9ed30a65333099252e
garyb9 <gberkinfand@gmail.com>
2022-07-25 03:08:02 +0300

    half solution of compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
+const privateKeyToPublicKey = require('ethereum-private-key-to-public-key');
+const publicKeyToAddress = require('ethereum-public-key-to-address');
 
 describe('Compromised challenge', function () {
 
@@ -61,6 +63,44 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+        
+        stringsFromServer = [
+            `4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35`,
+            `4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34`,
+        ]
+
+        // The leaked codes can be translated from hex codes -> ascii
+        let base64 = stringsFromServer.map(
+            element => Buffer.from(element.split(` `).join(``), `hex`).toString(`utf8`)
+        )
+
+        // then, its base64 -> hex string (private key)
+        let privateKeys = base64.map(
+            element => Buffer.from(element, `base64`).toString(`utf8`)
+        )
+        
+        // then, we get the public keys
+        let publicKeys = privateKeys.map(
+            element => privateKeyToPublicKey(element).toString('hex')
+        )
+        
+        // lasty we get the addresses
+        let addresses = publicKeys.map(
+            element => publicKeyToAddress(element).toString('hex')
+        )
+        console.log(addresses);
+        
+
+        // AttackContract = await ethers.getContractFactory('AttackCompromised', attacker);
+        // attackContract = await AttackContract.deploy(
+        //     this.pool.address,
+        // );
+        // await attackContract.executeFlashloan(TOKENS_IN_POOL);
+
+        // await ethers.provider.send("evm_increaseTime", [2 * 24 * 60 * 60]); // wait 2 days
+
+        // await attackContract.executeDrain();
+
     });
 
     after(async function () {
